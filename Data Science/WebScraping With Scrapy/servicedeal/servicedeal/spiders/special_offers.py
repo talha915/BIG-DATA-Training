9b973639d056a1131bd6ed9e70e430dba7b34354
talha915 <talhazafar915426@gmail.com>
2020-12-31 19:09:43 +0500

    Service Scraper Starting

---
+++ Data Science/WebScraping With Scrapy/servicedeal/servicedeal/spiders/special_offers.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class SpecialOffersSpider(scrapy.Spider):
+    name = 'special_offers'
+    allowed_domains = ['www.servis.com/product-category/men/']
+    start_urls = ['http://www.servis.com/product-category/men//']
+
+    def parse(self, response):
+        pass
