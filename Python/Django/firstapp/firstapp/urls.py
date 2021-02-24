f969435ebb9c4444f06b97e4a76b144fabf98c30
talha915 <talhazafar915426@gmail.com>
2021-02-24 15:10:32 +0500

    Todo API Rest Complete

--- Python/Django/firstapp/firstapp/urls.py
+++ Python/Django/firstapp/firstapp/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from todo import views
+
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoView, 'todo')
 
 urlpatterns = [
-    path('', include('calc.urls')),
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
