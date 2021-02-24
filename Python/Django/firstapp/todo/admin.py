c73aa9e10f66251e6d74b95b9d62cd0cfbaf91a9
talha915 <talhazafar915426@gmail.com>
2021-02-24 14:46:20 +0500

    Todo Admin

--- Python/Django/firstapp/todo/admin.py
+++ Python/Django/firstapp/todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import Todo
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+
 
 # Register your models here.
+admin.site.register(Todo, TodoAdmin)
+
+
