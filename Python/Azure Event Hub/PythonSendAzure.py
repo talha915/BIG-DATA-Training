638b41dcb90d539438af9045533a153196d68067
Talha Zafar <talhazafar915426@gmail.com>
2023-01-05 16:42:12 +0100

    Code commiting new

--- Python/Azure Event Hub/PythonSendAzure.py
+++ Python/Azure Event Hub/PythonSendAzure.py
@@ -9,9 +9,47 @@ import datetime
 connection_str = ""
 eventhub_name = ""
 
-test = si.get_quote_data('msft')
-test_df = pd.DataFrame([test], columns=test.keys())[['regularMarketTime', 'regularMarketPrice', 'marketCap', 'exchange', 'averageAnalystRating']]
-test_df['marketCap'] = test_df.apply(lambda row: "$"+str(round(row['marketCap'] / 1000000000000, 2))+"MM", axis=1)
-test_df['averageAnalystRating'] = test_df['averageAnalystRating'].str.split('-', 1, expand=True)
-test_df['regularMarketTime'] = datetime.datetime.fromtimestamp(test_df['regularMarketTime'])
-print(test_df.head(5))
+# test = si.get_quote_data('msft')
+# test_df = pd.DataFrame([test], columns=test.keys())[['regularMarketTime', 'regularMarketPrice', 'marketCap', 'exchange', 'averageAnalystRating']]
+# test_df['marketCap'] = test_df.apply(lambda row: "$"+str(round(row['marketCap'] / 1000000000000, 2))+"MM", axis=1)
+# test_df[['analystRating', 'analystBuySell']] = test_df['averageAnalystRating'].str.split('-', 1, expand=True)
+# test_df['regularMarketTime'] = datetime.datetime.fromtimestamp(test_df['regularMarketTime'])
+
+
+def get_data_for_stock(data):
+    df = pd.DataFrame([data], columns=data.keys())[['regularMarketTime', 'regularMarketPrice', 'marketCap', 'exchange', 'averageAnalystRating']]
+    df['marketCap'] = df.apply(lambda row: "$"+str(round(row['marketCap'] / 1000000000000, 2))+"MM", axis=1)
+    # df['averageAnalystRating'] = df['averageAnalystRating'].str.split('-', 1, expand=True)
+    df[['analystRating', 'analystBuySell']] = df['averageAnalystRating'].str.split('-', 1, expand=True)
+    df.drop('averageAnalystRating', axis=1, inplace=True)
+    df['regularMarketTime'] = datetime.datetime.fromtimestamp(df['regularMarketTime'])
+    return df.to_dict('record')
+
+
+# result = get_data_for_stock(si.get_quote_data('msft'))
+# print(result)
+
+
+async def run():
+    # Create a producer client to send messages to the event hub.
+    # Specify a connection string to your event hubs namespace and
+    # the event hub name.
+    try:
+        while True:
+            await asyncio.sleep(5)
+            producer = EventHubProducerClient.from_connection_string(conn_str=connection_str, eventhub_name=eventhub_name)
+            async with producer:
+                # Create a batch.
+                event_data_batch = await producer.create_batch()
+
+                # Add events to the batch.
+                event_data_batch.add(EventData(json.dumps(get_data_for_stock('msft'))))
+
+                # Send the batch of events to the event hub.
+                await producer.send_batch(event_data_batch)
+                print("Successfully send to azure event hub")
+    except Exception as e:
+        print("Couldn't send to azure event hub: ", e)
+
+
+loop = asyncio.get
