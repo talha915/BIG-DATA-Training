6f3a8c026d5b02a694b2ac42d92896f67da80cdf
Talha Zafar <talhazafar915426@gmail.com>
2023-01-05 16:17:00 +0100

    Code commiting

--- Python/Azure Event Hub/PythonSendAzure.py
+++ Python/Azure Event Hub/PythonSendAzure.py
@@ -1,10 +1,17 @@
-import pandas as pandas
+import pandas as pd
 import json
 from yahoo_fin import stock_info as si 
 from azure.eventhub.aio import EventHubProducerClient
 from azure.eventhub import EventData
 import asyncio
+import datetime
 
 connection_str = ""
 eventhub_name = ""
 
+test = si.get_quote_data('msft')
+test_df = pd.DataFrame([test], columns=test.keys())[['regularMarketTime', 'regularMarketPrice', 'marketCap', 'exchange', 'averageAnalystRating']]
+test_df['marketCap'] = test_df.apply(lambda row: "$"+str(round(row['marketCap'] / 1000000000000, 2))+"MM", axis=1)
+test_df['averageAnalystRating'] = test_df['averageAnalystRating'].str.split('-', 1, expand=True)
+test_df['regularMarketTime'] = datetime.datetime.fromtimestamp(test_df['regularMarketTime'])
+print(test_df.head(5))
