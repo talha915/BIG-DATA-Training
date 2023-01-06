3312cccb91433cee109deb457881d7e97d3bf9de
Talha Zafar <talhazafar915426@gmail.com>
2023-01-06 12:11:11 +0100

    Adding Stream Analytics Job

--- Python/Azure Event Hub/Azure Terraform/main.tf
+++ Python/Azure Event Hub/Azure Terraform/main.tf
@@ -54,3 +54,28 @@ resource "azurerm_eventhub" "examplehub" {
   partition_count     = 2
   message_retention   = 1
 }
+
+
+resource "azurerm_stream_analytics_job" "example_stream_analytics_job" {
+  name                                     = "example-job"
+  resource_group_name                      = azurerm_resource_group.example.name
+  location                                 = azurerm_resource_group.example.location
+  compatibility_level                      = "1.2"
+  data_locale                              = "en-GB"
+  events_late_arrival_max_delay_in_seconds = 60
+  events_out_of_order_max_delay_in_seconds = 50
+  events_out_of_order_policy               = "Adjust"
+  output_error_policy                      = "Drop"
+  streaming_units                          = 1
+
+  tags = {
+    environment = "Example"
+  }
+
+  transformation_query = <<QUERY
+    SELECT *
+    INTO [YourOutputAlias]
+    FROM [YourInputAlias]
+QUERY
+
+}
