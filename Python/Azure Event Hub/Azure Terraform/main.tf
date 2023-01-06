baa3e758068512fdd299a06c1f767a6517a938f3
Talha Zafar <talhazafar915426@gmail.com>
2023-01-06 01:55:36 +0100

    terraform creating Azure Data Lake Gen2

--- Python/Azure Event Hub/Azure Terraform/main.tf
+++ Python/Azure Event Hub/Azure Terraform/main.tf
@@ -15,3 +15,22 @@ resource "azurerm_resource_group" "example" {
   name     = "testing_first_time_group"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "testingfirstgen2lake"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
+  name               = "example"
+  storage_account_id = azurerm_storage_account.example.id
+
+  properties = {
+    hello = "aGVsbG8="
+  }
+}
