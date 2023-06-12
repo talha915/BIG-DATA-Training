5c6bdbf9a2b99264fa6f9ec7c66f7411b1649156
Talha Zafar <talhazafar915426@gmail.com>
2023-06-12 20:56:37 +0200

    Filter rows

--- Polars/first_practise.py
+++ Polars/first_practise.py
@@ -10,3 +10,17 @@ df = pl.DataFrame(
 )
 
 print(df)
+
+
+def filter_rows(df):
+    """
+    Input: Dataframe
+    Action: Filter the DataFrame to include only rows where the value in column "A" is greater than 2.
+    Output: Filtered rows
+    """
+    filtered_rows = df.filter(pl.col("A") > 2)
+    return filtered_rows
+
+filtered_rows = filter_rows(df)
+
+print(filtered_rows)
