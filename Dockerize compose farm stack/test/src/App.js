0f9f009ea44207a7c8de74f3d323db1c475ea31f
Talha Zafar <talhazafar915426@gmail.com>
2023-06-17 00:22:27 +0200

    docker-compose.yaml

--- Dockerize compose farm stack/test/src/App.js
+++ Dockerize compose farm stack/test/src/App.js
@@ -3,16 +3,28 @@ import './App.css';
 import { useEffect } from 'react';
 
 function App() {
-  // useEffect(()=> {
-  //   fetch('localhost:8000/dummy-result')
-  //   .then((res)=> res.json())
-  //   .then(
-  //     console.log("Res: ", res)
-  //   )
-  // }, []);
+  const [data, setData] = useState(null);
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/dummy-result');  
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
-      
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading data...</p>
+      )}
     </div>
   );
 }
