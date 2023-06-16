fa0f12faf88cc9c727e790da303951c351e2a18c
Talha Zafar <talhazafar915426@gmail.com>
2023-06-17 00:15:21 +0200

    farm stack dockerize

---
+++ Dockerize compose farm stack/test/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+  // useEffect(()=> {
+  //   fetch('localhost:8000/dummy-result')
+  //   .then((res)=> res.json())
+  //   .then(
+  //     console.log("Res: ", res)
+  //   )
+  // }, []);
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
