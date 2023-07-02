e63366aee0beb642c03a61fcbf8958e29584d46f
Talha Zafar <talhazafar915426@gmail.com>
2023-07-03 01:40:31 +0200

    new

--- Kafka/Assignment/simple-producer-and-consumer/producer.py
+++ Kafka/Assignment/simple-producer-and-consumer/producer.py
@@ -1,24 +1,27 @@
 from kafka import KafkaProducer
 
-# Kafka broker details
-bootstrap_servers = 'localhost:9092'
-topic = 'test_topic'
+def producer():
+    # Kafka broker details
+    bootstrap_servers = 'localhost:9092'
+    topic = 'test_topic'
 
-# Create a Kafka producer
-producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+    # Create a Kafka producer
+    producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
-# Produce messages
-messages = [
-    b'Ho 1',
-    b'Hi 2',
-    b'HoHI 3',
-    b'Oye'
-]
+    # Produce messages
+    messages = [
+        b'Ho 1',
+        b'Hi 2',
+        b'HoHI 3',
+        b'Oye'
+    ]
 
-for message in messages:
-    print(topic, message)
-    producer.send(topic, message)
+    for message in messages:
+        print(topic, message)
+        producer.send(topic, message)
 
-# Flush and close the producer
-producer.flush()
-producer.close()
+    # Flush and close the producer
+    producer.flush()
+    producer.close()
+
+producer()    
