b059b22360061060d59e753fc1ba84f553c3f5d1
Talha Zafar <talhazafar915426@gmail.com>
2023-07-06 11:21:58 +0200

    test_cases

--- FastApi/fastapi-ci-cd/test/test_index.py
+++ FastApi/fastapi-ci-cd/test/test_index.py
@@ -1,11 +1,11 @@
 from fastapi.testclient import TestClient
 import pytest
-from app.index import main
+from app.index import app
 
-client = TestClient(main)
+client = TestClient(app)
 
 
-def test_root():
+def test_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, FastAPI!"}
+    assert response.json() == {"message": "Hello, FastAPIs!"}
