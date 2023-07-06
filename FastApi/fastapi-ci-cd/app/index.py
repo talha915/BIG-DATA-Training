1974cb868bebc78badd8488b4f52e0edadfab668
Talha Zafar <talhazafar915426@gmail.com>
2023-07-06 02:39:08 +0200

    new

---
+++ FastApi/fastapi-ci-cd/app/index.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
