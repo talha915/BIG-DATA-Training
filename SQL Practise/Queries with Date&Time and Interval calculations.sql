d173344f328c4b2bc55bd032fea59046ae0fa97f
Talha Zafar <talhazafar915426@gmail.com>
2023-07-20 01:13:28 +0200

    Date Subtraction

--- SQL Practise/Queries with Date&Time and Interval calculations.sql
+++ SQL Practise/Queries with Date&Time and Interval calculations.sql
@@ -10,3 +10,6 @@ select datediff(now(), '2021/06/01') as date_diff;
 
 -- Date Addition
 SELECT DATE_ADD('2023-07-20', INTERVAL 7 DAY) as date_after_7_days;
+
+-- Date Subtraction
+select date_sub('2023-07-20', INTERVAL 12 DAY) AS DATE_BEFORE_12_DAYS; 
