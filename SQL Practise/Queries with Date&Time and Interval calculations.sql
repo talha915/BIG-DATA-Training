3d7f63638e416678d115287aff3441d316c7dcf5
Talha Zafar <talhazafar915426@gmail.com>
2023-07-20 01:11:50 +0200

    date difference

--- SQL Practise/Queries with Date&Time and Interval calculations.sql
+++ SQL Practise/Queries with Date&Time and Interval calculations.sql
@@ -7,3 +7,6 @@ select now() as current_time_date;
 SELECT DATEDIFF('2023-06-01', '2022-06-01') as date_diff; 
 
 select datediff(now(), '2021/06/01') as date_diff;
+
+-- Date Addition
+SELECT DATE_ADD('2023-07-20', INTERVAL 7 DAY) as date_after_7_days;
