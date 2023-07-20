3eca7c29be2bc9767aac973b4a36933c64b7dbc0
Talha Zafar <talhazafar915426@gmail.com>
2023-07-20 13:04:09 +0200

    window functions sum

--- SQL Practise/Queries with Window Functions and Aggregate Functions(49).sql
+++
@@ -1,9 +0,0 @@
-select * from classicmodels.orderdetails limit 10;
-
--- Row Number 
-select *, row_number() over (order by priceEach desc) as row_num
-from classicmodels.orderdetails;
-
--- WIndow function sum
-select *, sum(priceEach) over(order by orderNumber) as total_sum
-from classicmodels.orderdetails;
