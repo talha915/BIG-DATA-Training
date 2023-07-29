09230724882a407b8aa501c74f9ee135941ca6e3
Talha Zafar <talhazafar915426@gmail.com>
2023-07-29 20:32:16 +0200

    Find users who never posted single photo on instagram

--- SQL Practise/project/instagram/instagram analysis.sql
+++ SQL Practise/project/instagram/instagram analysis.sql
@@ -4,6 +4,13 @@ show tables;
 
 describe users;
 
--- Find 5 oldest users of instagram from database provided
+-- 1) Find 5 oldest users of instagram from database provided
 
 select * from users order by created_at limit 5; 
+
+-- 2) Find users who never posted single photo on instagram
+-- select * from photos limit 10;
+
+select * from users u left join photos p 
+on u.id = p.user_id where image_url is NULL order by u.username;
+
