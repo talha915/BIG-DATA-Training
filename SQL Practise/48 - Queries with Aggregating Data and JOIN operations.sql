f4c6223e8f0ffd14bf79634472a6a060a0afffd5
Talha Zafar <talhazafar915426@gmail.com>
2023-08-02 01:28:15 +0200

    queries practise start

--- SQL Practise/48 - Queries with Aggregating Data and JOIN operations.sql
+++ SQL Practise/48 - Queries with Aggregating Data and JOIN operations.sql
@@ -2,3 +2,17 @@ create database testing_db;
 
 use testing_db;
 
+create table customers(
+    CustomerID int not null primary key,
+    CustomerName varchar(50),	
+    Country varchar(50)
+);
+
+insert into customers
+(CustomerID, CustomerName, Country)
+values
+(1,'John','USA'),
+(2,'Alice','Canada'),
+(3,'Bob','UK'),
+(4,'Eve','Australia'),
+(5,'Carl','Germany');
