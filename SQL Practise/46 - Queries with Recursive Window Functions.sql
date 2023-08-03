1d796eddfde5e906d3987bd7aac506e8463999f1
Talha Zafar <talhazafar915426@gmail.com>
2023-08-03 15:01:57 +0200

    Query with Recursion

---
+++ SQL Practise/46 - Queries with Recursive Window Functions.sql
@@ -0,0 +1,23 @@
+WITH RECURSIVE numbers AS (
+  SELECT 1 AS n -- Initial Condition
+  UNION ALL
+  SELECT n + 1 FROM numbers WHERE n < 10 -- Recursive Condition
+)
+
+SELECT * FROM numbers;
+
+WITH RECURSIVE numbers AS (
+	SELECT 2 AS recursive_number
+    UNION ALL
+    SELECT recursive_number * 2 FROM numbers WHERE recursive_number <= 10
+)
+SELECT * FROM numbers;
+
+
+
+WITH RECURSIVE table_of_2 AS(
+	SELECT 1 AS counter, 2 AS base_number
+    UNION ALL
+    SELECT counter + 1, base_number from table_of_2 where counter < 10
+)
+SELECT base_number, counter, base_number*counter as multiple from table_of_2;
